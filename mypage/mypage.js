f4de6c48eb72bc1312489800ac74123b94d4c3ee
smilen26 <nakagawasumire@nakagawasumire-no-MacBook-Pro.local>
2013-10-11 23:21:44 +0900

    jsを変更しました

---
+++ mypage/mypage.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+		$('.myname').html("名前");
+
+		$('.followcount').append("(フォロー数)");
+		$('.followercount').append("(フォロワー数)");
+
+		$('#myintro').append("ここに自己紹介文が入ります");
+});
