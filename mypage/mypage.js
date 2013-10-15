086a31c487bb09d014a5cdadf91e043392ab5736
smilen26 <nakagawasumire@nakagawasumire-no-MacBook-Pro.local>
2013-10-15 22:02:02 +0900

    タブのアイコンと背景色を変更しました

--- mypage/mypage.js
+++ mypage/mypage.js
@@ -1,8 +1,18 @@
 $(document).ready(function(){
-		$('.myname').html("名前");
+	$('.myname').html("名前");
 
-		$('.followcount').append("30");
-		$('.followercount').append("25");
+	$('.followcount').append("30");
+	$('.followercount').append("25");
 
-		$('#myintro').append("鶴川民のししゃも信者鶴川民のししゃも信者鶴川民のししゃも信者鶴川民のししゃも信者鶴川民のししゃも信者鶴川民のししゃも信者鶴川民のししゃも信者鶴川民のししゃも信者鶴川民のししゃも信者鶴川民のししゃも信者鶴川民のししゃも信者鶴川民のししゃも信者鶴川民のししゃも信者鶴川民のししゃも信者");
+	$('#myintro').append("鶴川民のししゃも信者鶴川民のししゃも信者鶴川民のししゃも信者鶴川民のししゃも信者鶴川民のししゃも信者鶴川民のししゃも信者鶴川民のししゃも信者鶴川民のししゃも信者鶴川民のししゃも信者鶴川民のししゃも信者鶴川民のししゃも信者鶴川民のししゃも信者鶴川民のししゃも信者鶴川民のししゃも信者");
+});
+
+$(function(){
+	$('a img').hover(function(){
+		$(this).attr('src', $(this).attr('src').replace('_off', '_on'));
+	}, function(){
+		if (!$(this).hasClass('current')) {
+			$(this).attr('src', $(this).attr('src').replace('_on', '_off'));
+		}
+	});
 });
