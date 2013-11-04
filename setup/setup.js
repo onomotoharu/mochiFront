e6d0181788eb6b1c51e53ed6c3bf9257b59c3a39
smilen26 <nakagawasumire@nakagawasumire-no-MacBook-Pro.local>
2013-11-04 20:24:43 +0900

    テンプレ使いました

---
+++ setup/setup.js
@@ -0,0 +1,14 @@
+function footer() {
+	document.write ('<nav id="footer" name="nav">\n');      
+	document.write ('<ul>\n');       
+	document.write ('<li><a href=""><img src="../img/off/reco_off.png"></a></li>\n');    
+	document.write ('<li><a href=""><img src="../img/off/act_off.png"></a></li>\n');   
+	document.write ('<li><a href=""><img src="../img/off/my_off.png"></a></li>\n'); 
+	document.write ('<li><a href=""><img src="../img/on/set_on.png"></a></li>\n'); 
+	document.write ('</ul>\n');
+	document.write ('</nav>\n');     
+}
+
+$(document).ready(function(){
+	$('#pagename').append("設定")
+});
