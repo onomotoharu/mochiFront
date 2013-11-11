e0954ce2e5c3858dff5c5fc99e4fbd5b3668a538
saomi23 <bechico@Macintosh.local>
2013-11-11 12:02:30 +0900

    touchイベント実装

--- survey/survey.js
+++ survey/survey.js
@@ -1,8 +1,45 @@
-$("#item1").css({
-opacity: '0.6',
-display: 'none',
-width: '100%',
-height: '100%',
-background: '#000',
-zIndex: '1'
-});
+// $(function() {
+// 	$("#item1").css({
+// 	opacity: '0.8',
+// 	background: '#000',
+// 	zIndex: '1'
+// 	});
+// 	$("#item1").click(function() {
+// 		$("").css('display', 'block');
+// 	});
+// 	$("#item1").click(function() {
+// 		$("#item1").css('display', 'none');
+// 	});
+// });
+
+
+(function() {
+
+	var boxEl       = null;
+	var boxSelectEl = null;
+
+	function cacheDOMs () {
+
+		boxEl      = document.getElementById('item1');
+		
+	};
+
+	function setEvents () {
+
+		boxEl.addEventListener("touchstart", function(e) {
+			boxEl.className = "boxSelect";
+		});
+		boxEl.addEventListener("touchmove", function(e) {
+			boxEl.className = "boxNonSelect";
+		});
+		boxEl.addEventListener("touchend", function(e) {
+			boxEl.className = "boxSelect";
+		});
+
+	}
+
+	document.addEventListener("DOMContentLoaded", function (e) {
+		cacheDOMs();
+		setEvents();
+	}, false);
+})();
