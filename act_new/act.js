0b9796ae2191421804767a0e351d66dc650276af
smilen26 <nakagawasumire@nakagawasumire-no-MacBook-Pro.local>
2013-11-13 10:46:57 +0900

    とりあえず現段階

--- act_new/act.js
+++ act_new/act.js
@@ -14,13 +14,29 @@ $(document).ready(function(){
 });
 
 $(function(){
-	$('.hyouji_btn').click(function(){
-		$('.hyouji_on').toggle();
-		//return false;
-		$('.hyouji_off').css("display", "none");
-	});
 	$('.com_btn').click(function(){
 		$('.com_formsend').toggle();
 		return false;
 	});
+	$('input').focus(function(){
+		$('.allcontents').css("margin", "45px 0 0 0 ");
+		$('.hyouji_btn').click(function(){
+			$('#header').css("position","fixed").css("top", "0");
+		});
+	});
+	$('.hyouji_btn').click(function(){
+		$('.hyouji_on').toggle();
+		$('.hyouji_off').css("display", "none");
+		$('input').css("margin-top", "16px")
+	});
 });
+
+//$(function(){
+//	$('.user_photo').append("<img src="./img/foodphoto.png">");
+//	if(){
+//		$('.camera img').css("display", "block");
+//		$('.user_photo img').css("display", "block");
+//	}
+//});
+
+
