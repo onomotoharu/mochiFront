8e816290411480b9256a304938e655179ff198c5
saomi23 <bechico@macintosh.lan>
2013-11-16 01:29:39 +0900

    setupと関係ないファイル消去

--- follow/follow.js
+++
@@ -1,9 +0,0 @@
-function header() {
-  document.write ('<nav id="header" name="nav">\n');      
-  document.write ('<ul>\n');       
-  document.write ('<li id="back_btn"><a href="">＜</a></li>\n');    
-  document.write ('<li id="pagename"></li>\n');   
-  document.write ('<li id="header_r"><a href="../search/index.html">■</a></li>\n');
-  document.write ('</ul>\n');
-  document.write ('</nav>\n');  
-}
