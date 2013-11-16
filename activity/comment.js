bbca9927919d15b35f645eb691858a6ed9c1a14d
saomi23 <bechico@macintosh.lan>
2013-11-16 16:24:04 +0900

    フォーム常時表示,コメント、イイネ消去,フッター消去,テキストスタイル変更

--- activity/comment.js
+++ activity/comment.js
@@ -8,23 +8,33 @@ $(function(){
 
 	$('#l_btn a').attr("href", "./index.html");
 
-	$('.com_btn').click(function(){
-		$('.com_formsend').toggle();
-		return false;
-	});
+	// $('.com_btn').click(function(){
+	// 	$('.com_formsend').toggle();
+	// 	return false;
+	// });
 	$('input').focus(function(){
 		$('.allcontents').css("margin", "45px 0 0 0 ");
 		$('.hyouji_btn').click(function(){
 			$('#header').css("position","fixed").css("top", "0");
 		});
 	});
-	$('.hyouji_btn').click(function(){
-		$('.hyouji_on').toggle();
-		$('.hyouji_off').css("display", "none");
-		$('input').css("margin-top", "16px")
+	// $('.hyouji_btn').click(function(){
+	// 	$('.hyouji_on').toggle();
+	// 	$('.hyouji_off').css("display", "none");
+	// 	$('input').css("margin-top", "16px")
 
-	});
+	// });
 });
+
+function footerStart(selector){
+  // #itemまでスクロール
+  $('html,body').animate({scrollTop: $(selector).offset().top},'slow');
+  //スクロールの着地点を生成
+}
+ //セレクタ指定
+footerStart(".com_formsend");
+
+
 
 jQuery(function($) {
 
