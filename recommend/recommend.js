243d7eea3adf3757b4107c3fa3362b44d9beab6e
smilen26 <nakagawasumire@nakagawasumire-no-MacBook-Pro.local>
2013-11-18 00:29:39 +0900

    フッターのリンク飛べるようになったはず

--- recommend/recommend.js
+++ recommend/recommend.js
@@ -7,14 +7,3 @@ $(document).ready(function(){
 $(function(){
   $('#nav_reco img').attr("src", "../img/on/reco_on.png");
 });
-
-function footer() {
-	document.write ('<nav id="footer" name="nav">\n');      
-	document.write ('<ul>\n');       
-	document.write ('<li id="nav_reco"><a href=""><img src="../img/off/reco_off.png"></a></li>\n');    
-	document.write ('<li id="nav_act"><a href=""><img src="../img/off/act_off.png"></a></li>\n');   
-	document.write ('<li id="nav_my"><a href=""><img src="../img/off/my_off.png"></a></li>\n'); 
-	document.write ('<li id="nav_set"><a href=""><img src="../img/off/set_off.png"></a></li>\n'); 
-	document.write ('</ul>\n');
-	document.write ('</nav>\n');     
-}
