49b45104af7b24a99e6cff66daa2e6e17071877e
saomi23 <bechico@Macintosh.local>
2013-11-22 19:27:26 +0900

    仮JSONデータが投稿される

--- activity/comment.js
+++ activity/comment.js
@@ -1,63 +1,30 @@
-
 $(document).ready(function(){
 	$('#pagename').append("みんなのごはん");
 });
 
 $(function(){
 
+  var commentObj = {
+    userId : "namaedesu",
+    comment : "oisu--"
+  };
+
 	$('#l_btn a').attr("href", "./index.html");
 
-	// $('.com_btn').click(function(){
-	// 	$('.com_formsend').toggle();
-	// 	return false;
-	// });
 	$('input').focus(function(){
 		$('.allcontents').css("margin", "45px 0 0 0 ");
 		$('.hyouji_btn').click(function(){
 			$('#header').css("position","fixed").css("top", "0");
 		});
 	});
-	// $('.hyouji_btn').click(function(){
-	// 	$('.hyouji_on').toggle();
-	// 	$('.hyouji_off').css("display", "none");
-	// 	$('input').css("margin-top", "16px")
 
-	// });
-});
+  $(".send").click(function(userId,comment) {
+    var newComment = $("<div></div>").appendTo('div.comment').addClass('come_com');
 
+    newComment.append($("<span/>").text(commentObj.userId).addClass('user_id'));
+    newComment.append($("<span/>").text(commentObj.comment).addClass('com_txt'));
 
-jQuery(function($) {
-
-  // var frag_com     = new Boolean(false);
-  var frag_iine     = new Boolean(false);
-
-
-  // // コメントボタン
-
-  // $(".com_btn").click(function(){
-  //   if(frag_com == false){
-  //     frag_com = true;
-  //     $(".com_btn img").attr("src","img/com_on.png");
-      
-  //   }else{
-  //     frag_com = false;
-  //     $(".com_btn img").attr("src","img/com_off.png");
-  //   }
-  // })
-
-  // イイネ！ボタン
-
-  $(".iine_btn").click(function(){
-    if(frag_iine == false){
-      frag_iine = true;
-      $(".iine_btn img").attr("src","img/good_on.png");
-      
-    }else{
-      frag_iine = false;
-      $(".iine_btn img").attr("src","img/good_off.png");
-    }
-  })
-
+  });
 
 });
 
