fb45c82ac79b93e90383b8de5c669c12c42f0926
Lisachaaaaaaan <LISA@nishitarisha-no-MacBook-Pro.local>
2013-11-27 00:40:25 +0900

    change

--- medal/medal.js
+++ medal/medal.js
@@ -8,25 +8,27 @@ App.signIn("ren","test",function(json){
 
 App.getOwnBadges(function(medals){	
 	console.log(JSON.parse(medals));
-	//JSON.parse(medals)[0].created_at.split(" ")[0]
-	medal = JSON.parse(medals);
-// 	debugger;
-	for(i=0 ; i<4 ; i++){
 	
+	//JSON.parse(medals)[0].created_at.split(" ")[0]
+	
+	medal = JSON.parse(medals);
+
+// 	debugger;
+	for(var i=0 ; i<medals.length ; i++){	
 	  //リストを追加
 	  $("#medalimg").append('<a class="zoom" href="" title=""></a></div><div id="coment"><div class="medallog"></div><div id ="getdate"></div><div class="medalintro"></div>');	  
-	  //メダルの画像とタイトルを挿入
-	  
-	  $("#medalimg a").each(function(i){
-	  $(this).append('<img src="medal_img/'+medal[i].picture_name+'">')
-	  .attr('title', 'medal[i].description.split(" ")[0]');
 	  }
+	  //メダルの画像とタイトルを挿入
+	  $("#medalimg a").each(function(i){
+	  	$(this).append('<img src="medal_img/'+medals[i].picture_name+'">')
+		.attr('title', 'medal[i].description.split(" ")[0]');
+	  });
 	  //バッジ名を挿入
-	  $(".medallog").append('<p class="b">'+medal.title+'</p>');
+	  $(".medallog").append('<p class="b">'+medals[i].title+'</p>');
 	  //メダル取得の日付を挿入
-	  $("#getdate").append(medal.created_at.split(" ")[0]); 
+	  $("#getdate").append(medals[i].created_at.split(" ")[0]); 
 	　//バッジ名を追加 
-	  $(".medalintro").append(medal.description);
+	  $(".medalintro").append(medals[i].description);
 
 
 
