7e6bd7f55eadc421346feae6bc96fd34f521800c
Lisachaaaaaaan <LISA@nishitarisha-no-MacBook-Pro.local>
2013-11-27 19:41:57 +0900

    j

--- login/signin.js
+++ login/signin.js
@@ -1,15 +1,10 @@
 
+$(function(){
+
+	App.signIn("ren","test",function(json){
+		console.log(json);
+	});
+
+})
 
 
-$("#button").click(function(){
-  username = $("form#username").var();
-  pass = $("form#password").var();
-
-  result = CheeseController.sign_in(usernamen,pass);
-
-  $("h1").text = result.recipe_name;
-
-  if(result==failed){
-    alert("NG";
-  }
-});
