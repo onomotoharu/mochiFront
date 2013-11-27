879d38a0f0ad62d80a1fc44f403e9f4016afcf36
smilen26 <nakagawasumire@nakagawasumire-no-MacBook-Pro.local>
2013-11-27 18:28:42 +0900

    通信部分追加

--- graph/graph.js
+++ graph/graph.js
@@ -1,21 +1,18 @@
 
+profile = [{"name":"takarabe",
+			"follow":"10",
+			"follower":"20",
+			"intro":"鶴川民のししゃも信者",
+			"icon":"./img/my_icon_user.png"}];
 
-$(document).ready(function(){
+$(function(){
 	$('#pagename').append("マイページ")
+	$('#myphoto img').attr("src",profile[0].icon);
+	$('#myintro').append(profile[0].intro);
 
-	$('.myname').html("名前");
 
-	$('.followcount').append("30");
-	$('.followercount').append("25");
+  	$('#nav_my img').attr("src", "../img/on/my_on.png");
 
-	$('#myintro').append("鶴川民のししゃも信者鶴川民のししゃも信者鶴川民のししゃも信者鶴川民のししゃも信者鶴川民のししゃも信者鶴川民のししゃも信者鶴川民のししゃも信者鶴川民のししゃも信者鶴川民のししゃも信者鶴川民のししゃも信者鶴川民のししゃも信者鶴川民のししゃも信者鶴川民のししゃも信者鶴川民のししゃも信者");
-});
-
-$(function(){
-  $('#nav_my img').attr("src", "../img/on/my_on.png");
-});
-
-$(function(){
 
 	$('#log a').hover(function(){
 		$('#log a img').attr('src', $('#log a img').attr('src').replace('_off', '_on'));
@@ -24,8 +21,6 @@ $(function(){
 			$('#log a img').attr('src', $('#log a img').attr('src').replace('_on', '_off'));
 		}
 	});
-	
-
 	$('#graph a').hover(function(){
 		$('#graph a img').attr('src', $('#graph a img').attr('src').replace('_off', '_on'));
 	}, function(){
@@ -33,15 +28,13 @@ $(function(){
 			$('#graph a img').attr('src', $('#graph a img').attr('src').replace('_on', '_off'));
 		}
 	});
-
 	$('#fav a').hover(function(){
 		$('#fav a img').attr('src', $('#fav a img').attr('src').replace('_off', '_on'));
 	}, function(){
 		if (!$('#fav a img').hasClass('current')) {
 			$('#fav a img').attr('src', $('#fav a img').attr('src').replace('_on', '_off'));
 		}
-	});
-	
+	});	
 	$('#badge a').hover(function(){
 		$('#badge a img').attr('src', $('#badge a img').attr('src').replace('_off', '_on'));
 	}, function(){
@@ -49,5 +42,19 @@ $(function(){
 			$('#badge a img').attr('src', $('#badge a img').attr('src').replace('_on', '_off'));
 		}
 	});
+
+	App = new CheeseController();
+
+    App.signIn("ren","test",function(json){
+        console.log(json);
+    });
+
+    App.getOwnProfile(function(myprofile){
+        console.log(myprofile);
+        $('.myname').html(myprofile.screen_id);
+		$('.followcount').append(myprofile.following.length);
+		$('.followercount').append(myprofile.followers.length);
+        
+    });
 });
 
