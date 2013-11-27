3138a62ae42fede02119b26c06bb8fbf203f1ca2
hirayama tomoya <tommyhikki@gmail.com>
2013-11-27 18:23:55 +0900

    followjsいじり

--- follow/follow.js
+++ follow/follow.js
@@ -19,19 +19,19 @@ $(function(){
 	// 	console.log(json);
 	 });
 
-	App.setFollow(function(follow){	
-	console.log(follow);
-		for(var i=0; i<follow.length; i++){
+	App.getOwnProfile(function(data){	
+	console.log(data);
+		for(var i=0; i<data.following.length; i++){
 	 			 //リストを追加
-	  $(".allcontents").append('<div class="follow"><div class="follow_pic"><ul><li><a href=""></a></li></ul></div>                                                 <a class="account"></a><div class="follow_btn"></div></div>');
+	  $(".allcontents").append('<div class="follow"><div class="follow_pic"><ul><li><a href=""></a></li></ul></div><a class="account"></a><div class="follow_btn"></div></div>');
 	}
 	   //アカウント名を追加
     $("a.account").each(function(i){
-        $(this).append(follow[i].username);
+        $(this).append(data.following[i].screen_id);
     });
 	  //ユーザーの画像を挿入
 	  $(".follow_pic ul li a").each(function(i){
-        $(this).append('<img src="img/'+follow[i].userimg+'.png">');
+        $(this).append('<img src="img/'+data.following[i].photo+'.png">');
     });
 	  //フォローボタンを追加
 	   $(".follow_btn").each(function(i){
--- follow/follower.js
+++ follow/follower.js
@@ -4,29 +4,31 @@ $(function(){
 })
 
 
-var data=[
-{"username":"tomomi","userimg":"mob1"},
-{"username":"tommy","userimg":"mob2"},
-{"username":"becchi","userimg":"mob3"},
-{"username":"ando","userimg":"mob4"}
-]
-
 $(function(){
-	for(var i=0; i<data.length; i++){
+	App = new CheeseController();
+	// ## sample ##
+	 App.signIn("ren","test",function(json){
+	// 	console.log(json);
+	 });
+
+	App.getOwnProfile(function(data){	
+	console.log(data);
+		for(var i=0; i<data.followers.length; i++){
 	 			 //リストを追加
-	  $(".allcontents").append('<div class="follow"><div class="follow_pic"><ul><li><a href=""></a></li></ul></div>                                                 <a class="account"></a><div class="follow_btn"></div></div>');
+	  $(".allcontents").append('<div class="follow"><div class="follow_pic"><ul><li><a href=""></a></li></ul></div><a class="account"></a><div class="follow_btn"></div></div>');
 	}
 	   //アカウント名を追加
     $("a.account").each(function(i){
-        $(this).append(data[i].username);
+        $(this).append(data.followers[i].screen_id);
     });
 	  //ユーザーの画像を挿入
 	  $(".follow_pic ul li a").each(function(i){
-        $(this).append('<img src="img/'+data[i].userimg+'.png">');
+        $(this).append('<img src="img/'+data.followers[i].photo+'.png">');
     });
 	  //フォローボタンを追加
 	   $(".follow_btn").each(function(i){
         $(this).append('<ul><li><span class="toggleImage"><img src="img/follow3_on.png"></span></li></ul');
-    });
 
+})
+});
 });
