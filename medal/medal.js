8c104305245816f7a9e499204ba362fe5a591ef9
Lisachaaaaaaan <LISA@nishitarisha-no-MacBook-Pro.local>
2013-11-27 18:21:06 +0900

    バッジクリック後なくなること以外はでけた

--- medal/medal.js
+++ medal/medal.js
@@ -24,27 +24,28 @@ App.getOwnBadges(function(medals){
 	
 	for(var i=0 ; i < medals.length ; i++){	
 	  //リストを追加
-	  $(".tab_contents ul").append('<li><div class="medalimg"><a class="zoom" href="" title=""></a></div><div id="coment"><div class="medallog"></div><div id ="getdate"></div><div class="medalintro"></div></div>');
-	  
+	  $(".tab_contents").append('<ul><li><div class="medalimg"><a class="zoom" style="display:block;" href="" title=""></a></div><div id="coment"><div class="medallog"></div><div class ="getdate"></div><div class="medalintro"></div></div></li></ul>');
+	    };
 	  //メダルの画像とタイトルを挿入
 	  $(".medalimg a").each(function(i){
-	  	$(this).append('<img src="medal_img/'+medals[i].picture_name+'">');
+	  $(this).append('<img src="medal_img/'+medals[i].picture_name+'">');
 // 	  	$(this).attr("title", "medals[i].description.split(" ")[0]");
-	  });
+		});
 
 	  //バッジ名を挿入
 	  $(".medallog").each(function(i){
-	  	$(this).append('<p class="b">'+medals[i].title+'</p>')
+	  $(this).append('<p class="b">'+medals[i].title+'</p>');
 	  });
 	  
 	  //メダル取得の日付を挿入
- 	  $("#getdate").append(medals[i].created_at.split(" ")[0]); 
+ 	  $(".getdate").each(function(i){
+	  $(this).append(medals[i].created_at.split(" ")[0]); 
+	  });
 
 	　//バッジ名を追加 
- 	  $(".medalintro").append(medals[i].description);
-	  
-	  };
-
+ 	  $(".medalintro").each(function(i){
+	  $(this).append(medals[i].description);
+	  });
 	});
 
 });
