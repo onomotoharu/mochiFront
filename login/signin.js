c83c51ab6aaf93dda058e019b45a6130e7847ba8
Lisachaaaaaaan <LISA@nishitarisha-no-MacBook-Pro.local>
2013-11-27 22:02:08 +0900

    j

--- login/signin.js
+++ login/signin.js
@@ -1,10 +1,12 @@
 
-var name;
-var pass;
+$("#btn_login").click(function(){
+	
+	var screen_id = textarea;
+	var password = textarea;
+	
+	App.signIn("screen_id","password",function(signin){
+		console.log(signin);
 
-$(document).ready(function(){
-	App.signIn("name","pass",function(json){
-		console.log(json);
 	});
 });
 
