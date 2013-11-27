ace9d516fea7799726418002d1fb951e36cc80e3
saomi23 <bechico@Macintosh.local>
2013-11-27 16:38:14 +0900

    つくった日の日時を入れる

--- activity/comment.js
+++ activity/comment.js
@@ -15,7 +15,13 @@ $(function(){
   App.getDetail(1,function(recipe){
     $('.recipe_name a').text(recipe.name).attr({'href':recipe.source_url});
     $('.recipe_photo img').attr({'src':"http://winvelab.net/cheese/img/" + recipe.default_picture_name});
-    // $('.right .date').text(recipeData.cookedDay);
+  });
+
+  // つくった日
+  App.getOwnActivities(function(activity){
+    var date = activity.user[0].created_at;
+    $('.right .date').text(date);
+    console.log(activity);
   });
 
   // ユーザデータ
@@ -30,14 +36,16 @@ $(function(){
     });
   });
 
-  // コメントを投稿
+  // 送信ボタンをクリックしたら
   $(".send").click(function(userId,comment) {
 
-    var newComment = $("<div/>").appendTo('div.comment').addClass('come_com');
-
-    newComment.append($("<span/>").text(commentObj.userId).addClass('user_id'));
-    commentObj.comment = $(".com_input").val();
-    newComment.append($("<span/>").text(commentObj.comment).addClass('com_txt'));
+    // コメントを投稿
+    App.getOwnActivities(function(activity){
+      var newComment = $("<div/>").appendTo('div.comment').addClass('come_com');
+      newComment.append($("<span/>").text(commentObj.userId).addClass('user_id'));
+      commentObj.comment = $(".com_input").val();
+      newComment.append($("<span/>").text(commentObj.comment).addClass('com_txt'));
+    });
 
   });
 
