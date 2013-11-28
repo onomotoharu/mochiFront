ea6258891bb1c28724e200a2607b99d8b6aa7d7f
Lisachaaaaaaan <LISA@nishitarisha-no-MacBook-Pro.local>
2013-11-28 13:01:54 +0900

    fin

--- login/signin.js
+++ login/signin.js
@@ -1,13 +1,15 @@
-
-$("#btn_login").click(function(){
-	
-	var screen_id = textarea;
-	var password = textarea;
-	
+$(function(){
 	App.signIn("screen_id","password",function(signin){
 		console.log(signin);
-
-	});
+    });
 });
 
 
+$(function(){
+	$("#trigger").click(function(){
+    	var screen_id = $(".login_id").val();
+        var screen_ps = $(".login_ps").val();
+        alert(screen_id);
+        alert(screen_ps);
+    });
+});
