6cc0116ebc9984246bd5322b2325682a8f9fa983
saomi23 <bechico@Macintosh.local>
2013-11-28 18:47:04 +0900

    Revert 13fb0e1..3b808b8
    This rolls back to commit 13fb0e1578341e9bf27f4d219480d07181888c02.

--- activity/comment.js
+++ activity/comment.js
@@ -4,34 +4,46 @@ $(document).ready(function(){
 });
 
 $(function(){
+  var recipeData = {
+    recipeName : "おいしいよー",
+    recipePhoto : "./img/foodphoto.png",
+    recipeUrl : "../recipe/index.html",
+    cookedDay : "2013/12/22",
+  }
 
-  recipe_id    = getUrlVars()["recipe_id"];
-  acitivity_id = getUrlVars()["activity_id"];
+  var cookedUser = {
+    pic : "./img/my_icon_user.png",
+    id : "bechi"
+  }
 
+  var commentObj = {
+    userId : "namaedesu",
+    comment : ""
+  };
+  
   App = new CheeseController();
 
   App.signIn("ren","test",function(json){
     // console.log(json);
   });
 
-  // レシピデータ
-  App.getDetail(recipe_id,function(recipe){
+
+  App.getDetail(1,function(recipe){
+    console.log(recipe);
     $('.recipe_name a').text(recipe.name).attr({'href':recipe.source_url});
-    $('.recipe_photo img').attr({'src':"http://winvelab.net/cheese/img/" + recipe.default_picture_name});
   });
 
-  // つくった日
-  App.getOwnActivities(function(activity){
-    var date = activity.user[0].created_at;
-    $('.right .date').text(date.split("T")[0]);
-    console.log(activity);
-  });
+  // App.getOwnActivities(function(json){
 
+  // });
+
+  // レシピデータ
+  $('.recipe_photo img').attr({'src':recipeData.recipePhoto});
+  $('.right .date').text(recipeData.cookedDay);
   // ユーザデータ
-  // $('.user_info .user_icon img').attr({'src':localStorage.pic});
+  $('.user_info .user_icon img').attr({'src':cookedUser.pic});
   $('.user_info .user_id').text(localStorage.screen_id);
 
-  // inputの挙動
   $('input').focus(function(){
     $('.allcontents').css("margin", "45px 0 0 0 ");
     $('.hyouji_btn').click(function(){
@@ -39,16 +51,14 @@ $(function(){
     });
   });
 
-  // 送信ボタンをクリックしたら
-  $(".send").click(function() {
+  // コメントを投稿
+  $(".send").click(function(userId,comment) {
 
-    // コメントを投稿
-    App.getOwnActivities(function(activity){
-      var newComment = $("<div/>").appendTo('div.comment').addClass('come_com');
-      newComment.append($("<span/>").text(localStorage.screen_id).addClass('user_id'));
-      activity.user[0].comment = $(".com_input").val();
-      newComment.append($("<span/>").text(activity.user[0].comment).addClass('com_txt'));
-    });
+    var newComment = $("<div/>").appendTo('div.comment').addClass('come_com');
+
+    newComment.append($("<span/>").text(commentObj.userId).addClass('user_id'));
+    commentObj.comment = $(".com_input").val();
+    newComment.append($("<span/>").text(commentObj.comment).addClass('com_txt'));
 
   });
 
