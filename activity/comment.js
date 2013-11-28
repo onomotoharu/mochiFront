8819c163e15f833268aa8268037f9356c1a4b501
saomi23 <bechico@Macintosh.local>
2013-11-28 18:47:41 +0900

    Revert ace9d51..6cc0116
    This rolls back to commit ace9d516fea7799726418002d1fb951e36cc80e3.

--- activity/comment.js
+++ activity/comment.js
@@ -4,22 +4,6 @@ $(document).ready(function(){
 });
 
 $(function(){
-  var recipeData = {
-    recipeName : "おいしいよー",
-    recipePhoto : "./img/foodphoto.png",
-    recipeUrl : "../recipe/index.html",
-    cookedDay : "2013/12/22",
-  }
-
-  var cookedUser = {
-    pic : "./img/my_icon_user.png",
-    id : "bechi"
-  }
-
-  var commentObj = {
-    userId : "namaedesu",
-    comment : ""
-  };
   
   App = new CheeseController();
 
@@ -27,23 +11,24 @@ $(function(){
     // console.log(json);
   });
 
-
+  // レシピデータ
   App.getDetail(1,function(recipe){
-    console.log(recipe);
     $('.recipe_name a').text(recipe.name).attr({'href':recipe.source_url});
+    $('.recipe_photo img').attr({'src':"http://winvelab.net/cheese/img/" + recipe.default_picture_name});
   });
 
-  // App.getOwnActivities(function(json){
+  // つくった日
+  App.getOwnActivities(function(activity){
+    var date = activity.user[0].created_at;
+    $('.right .date').text(date);
+    console.log(activity);
+  });
 
-  // });
-
-  // レシピデータ
-  $('.recipe_photo img').attr({'src':recipeData.recipePhoto});
-  $('.right .date').text(recipeData.cookedDay);
   // ユーザデータ
-  $('.user_info .user_icon img').attr({'src':cookedUser.pic});
+  // $('.user_info .user_icon img').attr({'src':localStorage.pic});
   $('.user_info .user_id').text(localStorage.screen_id);
 
+  // inputの挙動
   $('input').focus(function(){
     $('.allcontents').css("margin", "45px 0 0 0 ");
     $('.hyouji_btn').click(function(){
@@ -51,14 +36,16 @@ $(function(){
     });
   });
 
-  // コメントを投稿
+  // 送信ボタンをクリックしたら
   $(".send").click(function(userId,comment) {
 
-    var newComment = $("<div/>").appendTo('div.comment').addClass('come_com');
-
-    newComment.append($("<span/>").text(commentObj.userId).addClass('user_id'));
-    commentObj.comment = $(".com_input").val();
-    newComment.append($("<span/>").text(commentObj.comment).addClass('com_txt'));
+    // コメントを投稿
+    App.getOwnActivities(function(activity){
+      var newComment = $("<div/>").appendTo('div.comment').addClass('come_com');
+      newComment.append($("<span/>").text(commentObj.userId).addClass('user_id'));
+      commentObj.comment = $(".com_input").val();
+      newComment.append($("<span/>").text(commentObj.comment).addClass('com_txt'));
+    });
 
   });
 
