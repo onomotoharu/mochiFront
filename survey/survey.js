9957f5fd730b72d208945f94286d835e20dcbbdb
ryukikobayasi <ryuki.yasu.mb4129@gmail.com>
2013-11-28 22:49:15 +0900

    追加

--- survey/survey.js
+++ survey/survey.js
@@ -8,12 +8,47 @@ jQuery(function(){
     } else if($(this).hasClass('box_on')) {
       $(this).addClass("box").removeClass('box_on');
        }
-  });
-　　$(function(){
-  $('#r_btn a').append("次へ")
-         .attr("href", "../recommend/index.html")
 
-         
-})
+    alert("クリックされました！"); 
+
+
+//box_onの配列
+    var recipe_id = [item1,item2,item3,item4,item5,item6,item7,item8];
+    console.log(recipe_id);  
+
+//データ送信
+    App = new CheeseController();
+    
+    App.sendMade(recipe_id,null);
+    console.log(recipe_id);  
+
+
+
+
+  });
+
+
+
+
+
 });
+
+$(function(){
+   $('#r_btn a').append("次へ")
+  .attr("href", "../recommend/index.html")
+
+
+  App = new CheeseController();
+
+  // 
+  App.signIn("ren","test",function(json){
+     console.log(json);
+    });
+
+  App.getDetail(function(recipe_id){
+     console.log(recipe_id);
+
+
+  });
+})
 
