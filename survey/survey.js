52edc966e4393376dfdc05f8365f8596622ff91c
ryukikobayasi <ryuki.yasu.mb4129@gmail.com>
2013-11-30 05:09:46 +0900

    tui

--- survey/survey.js
+++ survey/survey.js
@@ -7,25 +7,24 @@ jQuery(function(){
 	$(".box").click(function() {
     if($(this).hasClass('box')){
       $(this).addClass("box_on").removeClass('box');
-    } else if($(this).hasClass('box_on')) {
+    } 
+    else if($(this).hasClass('box_on')) {
       $(this).addClass("box").removeClass('box_on');
        
-       }
+    }
 
     alert("クリックされました！"); 
-
-
-//box_onの配列
-    var recipe_id = [item1,item2,item3,item4,item5,item6,item7,item8];
-    console.log(recipe_id);  
-
 //boxがクリックされたらデータ送信
-    App = new CheeseController();
+　 　　App = new CheeseController();
+　　　　App.signIn("ren","test",function(json){
+        console.log(json);
+  　　});
    
     App.sendMade(recipe_id,null);
-
-    console.log(recipe_id);  
-  });
+　　　　console.log(recipe_id);  
+  　　
+　　　　});
+　　
 
 });
 
@@ -33,24 +32,22 @@ jQuery(function(){
 $(function(){
    $('#r_btn a').append("次へ")
   .attr("href", "../recommend/index.html")
-
-
-   });
-
- // ページを開いたらデータを取得
-  App = new CheeseController();
-
-  App.getSample("ren","test",function(json){
-   console.log(json);
-
-     App.getSample(function(recipe_id){
-     console.log(recipe_id);
-
-  });
-
 })
 
+$(document).ready(function(){
+  
+  // ページを開いたらデータを取得
+  App = new CheeseController();
 
+  App.signIn("ren","test",function(json){
+        console.log(json);
+  });
 
+  App.getSample(function(suvey){
+     console.log(survey);
+  //$
+　　});
+
+});
 
 
