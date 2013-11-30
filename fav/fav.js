e1859781efacf99bcebdba5187054dfa7772c49f
saomi23 <bechico@macintosh.lan>
2013-11-30 21:44:48 +0900

    とりあえずお気に入りページ実装できた

--- fav/fav.js
+++ fav/fav.js
@@ -26,7 +26,7 @@ $(function(){
 		// お気に入りタイムラインDOM操作
 		for(i=0;i<myprofile.favorite_recipes.length;i++) {
 
-			var recipe_id = i;
+			var recipe_id = myprofile.favorite_recipes[i].id;
 			App.getDetail(recipe_id,function(recipe){
 
 				$fav_menu_photo = $('<img/>').addClass('fav_menu_photo').attr({'src':"http://winvelab.net/cheese/img/" + recipe.default_picture_name});
@@ -34,19 +34,26 @@ $(function(){
 
 				$money_img = $('<img/>').addClass('fav_icon').attr('src', './img/money.png');
 				$time_img = $('<img/>').addClass('fav_icon').attr('src', './img/time.png');
-				$menu_money = $('<span/>').addClass('fav_menu_money').text(recipe.required_money);
-				$menu_time = $('<span/>').addClass('fav_menu_time').text(recipe.necessary_time);
+				$menu_money = $('<span/>').addClass('fav_menu_money').text(recipe.required_money + "円");
+				$menu_time = $('<span/>').addClass('fav_menu_time').text(recipe.necessary_time + "分");
 				$border = $('<span/>').text(' | ');
 
-				$fav_menu      = $('<div/>').addClass('fav_menu').append($fav_menu_photo.after($fav_menu_title));
+				// シェアボタン生成
+		    	$share_img  = $('<img/>').attr('src', './img/…_off.png');
+		    	$open01     = $('<a href="#open01"></a>').append($share_img);
+		    	$p_open01   = $('<p/>').append($open01);
+		    	$share      = $('<div id="share"></div>').append($p_open01);
+
 				$fav_menu_info = $('<span/>').addClass('fav_menu_info').append($money_img.after($menu_money).after($border).after($time_img).after($menu_time));
-				$fav_info_all = $('<div/>').addClass('fav_info_all').append($fav_menu_all.after($share));
-				$('div.tab_title').after($fav_menu.after($fav_menu_info).after($fav_info_all));
+				$fav_info_all = $('<div/>').addClass('fav_info_all').append($fav_menu_info.after($share));
+				$fav_menu = $('<div/>').addClass('fav_menu').append($fav_menu_photo.after($fav_menu_title).after($fav_info_all));
+				$('div.tab_title').after($fav_menu);
+
+				$(".fav_menu_title,.fav_menu_photo").click(function() {
+				location.href = "../recipe/index.html?recipe_id=" + recipe_id;
 
 			});
 
-			$(".fav_menu_title,.fav_menu_photo").click(function() {
-				location.href = "../recipe/index.html?index.html" + recipe_id;
 			});
 		};
 
