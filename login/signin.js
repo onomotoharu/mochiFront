7e0b177c0434f01101c29716b65c85fb05ae7273
motoharu <onomotoharu@gmail.com>
2013-12-02 15:13:29 +0900

    modified template.js

--- login/signin.js
+++ login/signin.js
@@ -9,7 +9,7 @@ $(function(){
 	$(".btn_login").click(function(){
     	var screen_id = $(".login_id").val();
         var screen_ps = $(".login_ps").val();
-        alert(screen_id);
-        alert(screen_ps);
+        // alert(screen_id);
+        // alert(screen_ps);
     });
 });
