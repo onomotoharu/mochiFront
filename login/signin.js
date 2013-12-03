96eb16352525f1098b41e2b05966a4ba825fbd73
motoharu <onomotoharu@gmail.com>
2013-12-04 01:32:13 +0900

    fixed bugs.

--- login/signin.js
+++ login/signin.js
@@ -6,7 +6,6 @@ $(function(){
 	$(".btn_login").click(function(){
     	var screen_id = $(".login_id").val();
         var screen_ps = $(".login_ps").val();
-        // alert(screen_id);
-        // alert(screen_ps);
+        App.signIn(screen_id,screen_ps,function(a){console.log(a);});
     });
 });
--- login/signup.js
+++ login/signup.js
@@ -1,6 +1,7 @@
 $(function(){
 	$(".btn_login").click(function(){
     	var screen_id = $(".screen_id").val();
-        var password  = $(".password").val();
+      var password  = $(".password").val();
+      App.createUser(screen_id,password,null);
     });
 });
