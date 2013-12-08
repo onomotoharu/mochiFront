60762ef8052204db4501e9050a938b673799919d
smilen26 <nakagawasumire@nakagawasumire-no-MacBook-Pro.local>
2013-12-08 20:47:04 +0900

    グラフできました

--- graph/graph.js
+++ graph/graph.js
@@ -1,4 +1,6 @@
 
+var max=0;
+
 $(document).ready(function(){
 	$('#pagename').append("マイページ")
 
@@ -16,14 +18,87 @@ $(document).ready(function(){
 
 		App.getGraph(screen_id, function(graph){
 			console.log(graph);
-			/*$("#input1").attr("value", graph.category[0]);
+
+			for(i=0; i<graph.category.length; i++){	
+				if(max < graph.category[i]){
+					max=graph.category[i];
+				}
+			}
+
+			$("#input1").attr("value", graph.category[0]);
 			$("#input2").attr("value", graph.category[1]);
 			$("#input3").attr("value", graph.category[2]);
 			$("#input4").attr("value", graph.category[3]);
 			$("#input5").attr("value", graph.category[4]);
-			$("#input6").attr("value", graph.category[5]);*/
+			$("#input6").attr("value", graph.category[5]);
+			$("#input7, #input8, #input9, #input10, #input11, #input12").attr("value", max);
 		});
 	});
+
+	$('#canvasChart').canvasChart({
+        /*********************
+         * 基本設定
+        *********************/
+        polygon             : 6,            //頂点の数
+        chartInput          : '.chartInput',//チャートのインプットclass
+        valuation           : max,          //評価値
+        radius              : 130,          //半径
+        bgType              : 'pol',        //背景のタイプ　 pol:多角形 or arc:円
+        angle               : '30',         //レーダーチャートの回転度数
+            
+        /*********************
+         * 背景
+        *********************/
+        bgStroke            : true,         //背景の線 true or false
+        bgStrokeColor       : '#ece2d5',    //背景の線色
+        bgLineWidth         : 4,            //背景の線の太さ
+        bgFill              : true,        //背景の塗り true or false
+        bgDivision          : 3,                    //背景の分割数
+            
+        /*********************
+         * チャート
+        *********************/
+        chartStrokeColor    : ['#e07431','#9c7d25'],  //チャートの線
+        chartFillColor      : ['rgba(224, 116, 49, 0.3)', 'none'],//チャートの塗り色 noneで塗らない
+        chartLineWidth      : 6,//線の太さ
+            
+        /*********************
+         * レーダーチャートの頂点
+        *********************/
+        chartPoint          : true,         //チャートの点描画 true or false
+        chartPointType      : ['square','arc'],   //点の図形タイプ square:四角 or arc:円
+        chartPointType2     : ['fill'],     //線か塗りつぶし　stroke or fill
+        chartPointLineWidth : 1,            //頂点の線の太さ
+        chartPointSize      : [0, 15],         //チャートの頂点サイズ
+        chartPointColor     : [''],         //チャートの頂点描画色、指定無しで線と同じ色
+            
+        /*********************
+         * 評価名
+        *********************/
+        valuationPrint      : true,         //評価名の表示　true or false
+        valuationName       : ['麺','米','菜','魚','肉', '他'],//評価名
+        valuationPosition   : 1,            //評価名要素の位置
+        valuationClass      : '',           //評価名要素のclass デフォルトでvalName
+            
+        /**********************
+         評価の数値
+        **********************/
+        valuationCntPrint      : false,                //評価の数値表記 true or false
+        valuationCntClass       : '',           //評価の数値のclass　デフォルトでvalCntName
+        printZero           : false,        //中央0の表記 true or false
+        valuationCntDivision: 3,                    //評価の数値分割数
+                                    
+        /*********************
+         * 中央からのゲージライン
+        *********************/
+        gauge               : true,         //中央からのゲージライン描画 true or false
+        gaugeColor          : '',           //中央からのゲージライン描画色、指定無しで背景線と同じ色
+                                    
+        /*********************
+         * ゲージラインのスケール
+        *********************/
+        scale               : false,         //ゲージラインの目盛り描画 true or false
+    });
 
 	$('#log a').hover(function(){
 		$('#log a img').attr('src', $('#log a img').attr('src').replace('_off', '_on'));
